4dee94ffe50077f12d7ecb906a9d7f14169760e7
Ritikesh Patel <patelritikesh56@gmail.com>
2025-01-31 13:48:26 +0530

    Initial Commit

---
+++ RitikeshCard/RitikeshCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RitikeshCardApp.swift
+//  RitikeshCard
+//
+//  Created by RITIKESH PATEL on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RitikeshCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
